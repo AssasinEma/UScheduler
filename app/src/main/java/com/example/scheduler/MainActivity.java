52bd376c62915fe6ac1256e815c6fb541a8b29dc
AssasinEma <andrei.emanuel.marius@gmail.com>
2023-03-18 20:32:48 +0200

    Edit Text method input

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO: Implement the first things in an app
+
+
+
+    }
+
+    public void onButtclick(View view)
+    {
+
+        TextView var =findViewById(R.id.txtHello);
+        EditText txtvar = findViewById(R.id.editTxt);
+        txtvar.getText().toString();
+        var.setText(txtvar.getText().toString());
+
     }
 }
