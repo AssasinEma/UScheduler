be29f815e6fc302f1cda4ad730521e56d0e7c01f
AssasinEma <andrei.emanuel.marius@gmail.com>
2023-05-11 20:05:40 +0300

    Enrollment buttons done ( They do not do anything yet )

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.scheduler;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout parent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        //TODO: Implement the first things in an app
+        parent =  findViewById(R.id.Welcome);
+        Button enrollUser = findViewById(R.id.enroll);
 
-
+        enrollUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    enrollWithTxt();
+            }
+        });
 
     }
 
-    @SuppressLint("SetTextI18n")
-    public void onButtclick(View view) {
-
+    private void enrollWithTxt ()
+    {
+        TextView group = findViewById(R.id.editTxt);
+        if (TextUtils.isEmpty(group.getText()))
+            Snackbar.make(parent, "You have not selected any group!", Snackbar.LENGTH_SHORT).show();
+        else
+            Snackbar.make(parent, "You have enrolled in "+ group.getText().toString(), Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Next", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(MainActivity.this, "Next PAGE!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .show();
     }
 }
